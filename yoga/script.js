53a16f8089855c044c65000e446ccbd9c53a81de
YanaBakinovskaya <34380938+YanaBakinovskaya@users.noreply.github.com>
2019-05-03 01:01:46 +0300

    Add files via upload

---
+++ yoga/script.js
@@ -0,0 +1,95 @@
+//window.addEventListener('load');
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+  //tabs
+  function workWithTab(classNameParentTab, classNameTab, classNameTabContent) {
+    let info = document.querySelector(classNameParentTab),
+      tab = document.querySelectorAll(classNameTab),
+      tabContent = document.querySelectorAll(classNameTabContent);
+
+    function hideTabContent(a) {
+      for (let i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+      }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+      if (tabContent[b].classList.contains('hide')) {
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+      }
+    }
+
+    info.addEventListener('click', function (event) {
+      let target = event.target;
+      if (target && target.classList.contains('info-header-tab')) {
+        for (let i = 0; i < tab.length; i++) {
+          if (target == tab[i]) {
+            hideTabContent(0);
+            showTabContent(i);
+            break;
+          }
+        }
+      }
+    });
+  }
+
+  workWithTab('.info-header', '.info-header-tab', '.info-tabcontent');
+
+  //timer
+  let deadline = '2019-04-06';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / (1000 * 60 * 60)));
+
+    return {
+      'total': t,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+
+      function checkTime(i) {
+        if (i < 10) {
+          i = '0' + i;
+        }
+        return i;
+      }
+
+      function timerWithZero(elem, t) {
+        elem.textContent = checkTime(t);
+      }
+
+      timerWithZero(hours, t.hours);
+      timerWithZero(minutes, t.minutes);
+      timerWithZero(seconds, t.seconds);
+
+      if (t.total < 0) {
+        timerWithZero(hours, 0);
+        timerWithZero(minutes, 0);
+        timerWithZero(seconds, 0);
+        clearInterval(timeInterval);
+      }
+    }
+
+  }
+
+  setClock('timer', deadline);
+});
