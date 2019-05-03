4a1475a98bcf2fa15bdfb01d83c137868353446d
YanaBakinovskaya <34380938+YanaBakinovskaya@users.noreply.github.com>
2019-05-03 04:12:21 +0300

    Add files via upload

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,11 +1,11 @@
 //window.addEventListener('load');
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   'use strict';
-
+  //tabs
   function workWithTab(classNameParentTab, classNameTab, classNameTabContent) {
     let info = document.querySelector(classNameParentTab),
-        tab = document.querySelectorAll(classNameTab),
-        tabContent = document.querySelectorAll(classNameTabContent);
+      tab = document.querySelectorAll(classNameTab),
+      tabContent = document.querySelectorAll(classNameTabContent);
 
     function hideTabContent(a) {
       for (let i = a; i < tabContent.length; i++) {
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click', function (event) {
       let target = event.target;
       if (target && target.classList.contains('info-header-tab')) {
         for (let i = 0; i < tab.length; i++) {
@@ -36,6 +36,116 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     });
   }
-  
+
   workWithTab('.info-header', '.info-header-tab', '.info-tabcontent');
+
+  //timer
+  let deadline = '2019-04-06';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / (1000 * 60 * 60)));
+
+    return {
+      'total': t,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+
+      function checkTime(i) {
+        if (i < 10) {
+          i = '0' + i;
+        }
+        return i;
+      }
+
+      function timerWithZero(elem, t) {
+        elem.textContent = checkTime(t);
+      }
+
+      timerWithZero(hours, t.hours);
+      timerWithZero(minutes, t.minutes);
+      timerWithZero(seconds, t.seconds);
+
+      if (t.total < 0) {
+        timerWithZero(hours, 0);
+        timerWithZero(minutes, 0);
+        timerWithZero(seconds, 0);
+        clearInterval(timeInterval);
+      }
+    }
+
+  }
+
+  setClock('timer', deadline);
+
+  //scroll
+  let navLink = document.querySelectorAll('[href^="#"]'),
+      speed = 2;
+
+  for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener('click', function (event) {
+      event.preventDefault();
+      let w = window.pageYOffset,
+          hash = this.href.replace(/[^#]*(.*)/, '$1'),
+          t = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+      requestAnimationFrame(step);
+
+      function step(time) {
+        if (start === null) {
+          start = time;
+        }  
+        let progress = time - start,
+            r = (t < 0 ? Math.max(w - progress / speed, w + t) : Math.min(w + progress / speed, w + t));
+        window.scrollTo(0, r);
+        if (r != w + t) {
+          requestAnimationFrame(step);
+        } else {
+          location.hash = hash; // URL с хэшем
+        }
+      }
+    }, false);
+  }
+
+  // let navLink = document.getElementsByTagName('a'),
+  //     about = document.querySelector('#about').scrollTop;
+  // console.log(navLink[0]);
+  // console.log(navLink[0].getAttribute('href'));
+  // function scrollMenu() {
+
+  //   about.scrollIntoView({
+  //     block: "start",
+  //     behavior: "smooth"
+  //   });
+  // }
+  //navLink.addEventListener('click', scrollMenu);
+  //for (let i = 0; i < 4; i++) {
+    //let has = navLink[i].getAttribute('href').textContent;
+    //navLink[0].addEventListener('click', scrollMenu);
+      //let has = navLink[i].getAttribute('href').replace('#','');
+      
+        //console.log(has);
+        //about.scrollIntoView({behavior: "smooth"});
+      
+    //});
+  //}
+  // navLink[0].addEventListener('click', function() {
+  //   scrollTo(navLink[0], about);
+  // });
+
 });
